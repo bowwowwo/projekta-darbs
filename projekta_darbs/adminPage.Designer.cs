2fa799fbb6bdb2575851a1866d320ec972528b33
arnolds69 <berzins.arnolds@gmail.com>
2025-03-25 10:29:20 +0200

    adminpage database view refresh database un add key button1 vel vajag katram textboxam pateikt ko vajag vadit ieksa( man slinkums)

--- projekta_darbs/adminPage.Designer.cs
+++ projekta_darbs/adminPage.Designer.cs
@@ -28,44 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
-            materialComboBox1 = new MaterialSkin.Controls.MaterialComboBox();
+            dataGridView2 = new DataGridView();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
             SuspendLayout();
             // 
-            // materialComboBox1
+            // dataGridView2
             // 
-            materialComboBox1.AutoResize = false;
-            materialComboBox1.BackColor = Color.FromArgb(255, 255, 255);
-            materialComboBox1.Depth = 0;
-            materialComboBox1.DrawMode = DrawMode.OwnerDrawVariable;
-            materialComboBox1.DropDownHeight = 174;
-            materialComboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
-            materialComboBox1.DropDownWidth = 121;
-            materialComboBox1.Font = new Font("Roboto Medium", 14F, FontStyle.Bold, GraphicsUnit.Pixel);
-            materialComboBox1.ForeColor = Color.FromArgb(222, 0, 0, 0);
-            materialComboBox1.FormattingEnabled = true;
-            materialComboBox1.IntegralHeight = false;
-            materialComboBox1.ItemHeight = 43;
-            materialComboBox1.Location = new Point(234, 94);
-            materialComboBox1.MaxDropDownItems = 4;
-            materialComboBox1.MouseState = MaterialSkin.MouseState.OUT;
-            materialComboBox1.Name = "materialComboBox1";
-            materialComboBox1.Size = new Size(306, 49);
-            materialComboBox1.StartIndex = 0;
-            materialComboBox1.TabIndex = 0;
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Location = new Point(245, 67);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.Size = new Size(358, 377);
+            dataGridView2.TabIndex = 5;
+            dataGridView2.CellContentClick += dataGridView2_CellContentClick;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(6, 201);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 6;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(139, 201);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(58, 421);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(58, 392);
+            textBox4.Name = "textBox4";
+            textBox4.ReadOnly = true;
+            textBox4.Size = new Size(100, 23);
+            textBox4.TabIndex = 9;
+            textBox4.Text = "Atslegas ID";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(83, 148);
+            button1.Name = "button1";
+            button1.Size = new Size(100, 47);
+            button1.TabIndex = 10;
+            button1.Text = "pievieno atslegu";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.ForeColor = Color.Coral;
+            button2.Location = new Point(83, 326);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 40);
+            button2.TabIndex = 11;
+            button2.Text = "nonem atslegu";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(609, 201);
+            button3.Name = "button3";
+            button3.Size = new Size(100, 47);
+            button3.TabIndex = 12;
+            button3.Text = "refresh db";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
             // 
             // adminPage
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(materialComboBox1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(dataGridView2);
             Name = "adminPage";
             Text = "adminPage";
+            Load += adminPage_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
-        private MaterialSkin.Controls.MaterialComboBox materialComboBox1;
+
+
+        private DataGridView dataGridView2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Button button1;
+        private Button button2;
+        private Button button3;
     }
 }
--- projekta_darbs/adminPage.cs
+++ projekta_darbs/adminPage.cs
@@ -4,25 +4,103 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
+using System.Data.SQLite;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace projekta_darbs
 {
     public partial class adminPage : MaterialForm
     {
+        
+    
+
+
+
         public adminPage()
         {
+
+
             InitializeComponent();
             var materialSkinManager = MaterialSkinManager.Instance;
             materialSkinManager.AddFormToManage(this);
             materialSkinManager.Theme = MaterialSkinManager.Themes.DARK;
             materialSkinManager.ColorScheme = new ColorScheme(Primary.DeepPurple800, Primary.DeepPurple600, Primary.DeepPurple400, Accent.Blue200, TextShade.WHITE);
+
+           
+        }
+        string AtslegasID;
+        string AtslegasKabinets;
+
+
+        private void adminPage_Load(object sender, EventArgs e)
+        {
+
         }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
 
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+
+        {
+            AtslegasID = textBox1.Text;
+            AtslegasKabinets = textBox2.Text;
+
+            string exeDirectory = AppDomain.CurrentDomain.BaseDirectory;    // sql for login
+            string projectRootDirectory = Path.GetFullPath(Path.Combine(exeDirectory, @"..\..\..\"));
+            string dbFilePath = Path.Combine(projectRootDirectory, "db", "prog2atslgisnsys.db");
+            string connectionString = @"data source =" + dbFilePath;
+            SQLiteConnection con = new SQLiteConnection(connectionString);
+            con.Open();
+            string query = "INSERT INTO Atslegas (AtslegasID, AtslegasKabinets) VALUES (@AtslegasID, @AtslegasKabinets)";
+            using (SQLiteCommand cmd2 = new SQLiteCommand(query, con))
+            {
+                cmd2.Parameters.AddWithValue("@AtslegasID", AtslegasID);
+                cmd2.Parameters.AddWithValue("@AtslegasKabinets", AtslegasKabinets);
+                cmd2.ExecuteNonQuery();
+            }
+            MessageBox.Show("pielikam atslegu");
+            con.Close();
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string exeDirectory = AppDomain.CurrentDomain.BaseDirectory;    // sql for login
+            string projectRootDirectory = Path.GetFullPath(Path.Combine(exeDirectory, @"..\..\..\"));
+            string dbFilePath = Path.Combine(projectRootDirectory, "db", "prog2atslgisnsys.db");
+            string connectionString = @"data source =" + dbFilePath;
+            SQLiteConnection con = new SQLiteConnection(connectionString);
+            con.Open();
+            string query = "SELECT * FROM Atslegas";
+            SQLiteDataAdapter da = new SQLiteDataAdapter(query, con);
+            DataTable dt = new DataTable();
+
+            da.Fill(dt);
+            dataGridView2.DataSource = dt;
+            con.Close();
+          
+
+        }
     }
 }
